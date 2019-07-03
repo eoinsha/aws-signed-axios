71d4f7c1125567e030bcea81b6fa4e2b57d8d981
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-07-03 12:44:41 +0100

    Add test

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const AWS = require('aws-sdk')
 const aws4 = require('aws4')
 const axios = require('axios')
 
-module.exports = function signedAxios(request) {
+module.exports = function signedAxios (request) {
   const { host, pathname, search } = new URL(request.url)
   request.host = host
   request.path = pathname + search
+  request.headers = {
+    'Content-Type': 'application/json'
+  }
 
   const signedRequest = aws4.sign(request, {
     secretAccessKey: AWS.config.credentials.secretAccessKey,
@@ -17,5 +20,5 @@ module.exports = function signedAxios(request) {
 
   delete signedRequest.headers['Host']
   delete signedRequest.headers['Content-Length']
-  return axios(signedRequest)
+  return axios.request(signedRequest)
 }
---
+++ test/index.test.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const proxyquire = require('proxyquire')
+const { test } = require('tap')
+
+test('Request is signed', async t => {
+  AWS.config.credentials = {
+    secretAccessKey: 'dummy-secret',
+    accessKeyId: 'dummy-key'
+  }
+  const url = 'https://api.example.com/api/person'
+  const payload = {
+    name: 'Jo Bloggs',
+    age: 80
+  }
+
+  const received = {}
+
+  const signedAxios = proxyquire('..', {
+    axios: {
+      request: function mockRequest () {
+        received.requestArgs = arguments
+        return Promise.resolve({})
+      }
+    },
+    aws4: {
+      sign: function mockSign (request, options) {
+        received.signArgs = { request, options }
+        return request
+      }
+    }
+  })
+
+  const result = await signedAxios({
+    url,
+    method: 'POST',
+    data: payload
+  })
+
+  t.equal(received.signArgs.request.host, 'api.example.com')
+  t.same(received.requestArgs[0].data, payload)
+
+  t.same(result, {})
+  t.end()
+})
